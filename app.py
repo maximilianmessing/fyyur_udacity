57e74b8fd03b2863945c5c0f1740eaa693561ebb
Maximilian Messing <maximilian.messing@whu.edu>
2020-03-08 20:11:42 +0100

    change genre field

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean(), default=False)
     seeking_description = db.Column(db.String())
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     shows = db.relationship('Show', backref='venue', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate - CHECK
@@ -62,7 +62,7 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean(), default=False)
@@ -261,7 +261,7 @@ def create_venue_submission():
             city=request.form['city'],
             state=request.form['state'],
             address=request.form['address'],
-            genres=request.form['genres'],
+            genres=request.form.getlist('genres'),
             phone=request.form['phone'],
             facebook_link=request.form['facebook_link']
         )
@@ -491,7 +491,7 @@ def create_artist_submission():
             name=request.form['name'],
             city=request.form['city'],
             state=request.form['state'],
-            genres=request.form['genres'],
+            genres=request.form.getlist('genres'),
             phone=request.form['phone'],
             facebook_link=request.form['facebook_link']
         )
